00f1088135d93ce2079e42c25e69666201cdffa6
ParthaMalakar <parthamalakar4321@gmail.com>
2023-10-20 01:34:03 +0600

    delete api created for a product

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/user', async (req, res) => {
   res.send(result);
 });
 
-app.put('/user', async (req, res) => {
+app.patch('/user', async (req, res) => {
   const user = req.body;
   const filter = { email: user.email }
   const updateDoc = {
@@ -75,8 +75,17 @@ app.put('/user', async (req, res) => {
 
 app.get('/user/:email', async (req, res) => {
   const email = req.params.email;
+  console.log(email)
   const query = { email: email }
+  console.log(query)
   const result = await userCollection.findOne(query);
+  res.send(result);
+})
+
+app.delete('/user/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
   res.send(result);
 })
     // Send a ping to confirm a successful connection
